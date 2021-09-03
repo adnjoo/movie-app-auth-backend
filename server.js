e7ce5204d42d67a621af7291a0081901df890a13
Andrew Njoo <adnjoo@gmail.com>
2021-09-03 13:10:01 -0700

    formatting

--- server.js
+++ server.js
@@ -5,12 +5,10 @@ const controller = require("./controller.js");
 // const port = 4001; // use either the host env var port (PORT) provided by Heroku or the local port (4001) on your machine
 const port = process.env.PORT || 4001; // use either the host env var port (PORT) provided by Heroku or the local port (4001) on your machine
 
-
 const app = express(); //let app = a new express instance, and use CORS and JSON
 
 app.use(cors()); // Enable CORS
 app.use(express.json()); // Recognize Request Objects as JSON objects
-
 
 //add a movie
 // app.post("/", controller.addMovie);
@@ -30,13 +28,13 @@ app.listen(port, () => {
 });
 
 //register and login routes
-app.use('/auth', require('./auth-routes/jwtAuth'))
+app.use("/auth", require("./auth-routes/jwtAuth"));
 
 //dashboard route
-app.use('/dashboard', require('./auth-routes/dashboard'))
+app.use("/dashboard", require("./auth-routes/dashboard"));
 
 //new routes
-app.use('/', require('./controller2'))
+app.use("/", require("./controller2"));
 
 //help
 //https://medium.com/dataseries/add-timeout-capability-to-express-apps-with-connect-timeout-fce06d76e07a
